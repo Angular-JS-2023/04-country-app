9f191578f5e93a804c9fb3c451c0baacc80cad37
Luis Angel <angelfgdeveloper@gmail.com>
2023-08-13 15:41:26 -0600

    Estructura de paginas y input de SearchBox compartido entre modulos

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { CountriesRoutingModule } from './countries-routing.module';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
@@ -18,6 +20,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
   imports: [
     CommonModule,
     CountriesRoutingModule,
+    SharedModule,
   ]
 })
 export class CountriesModule { }
